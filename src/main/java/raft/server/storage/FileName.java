9c79c0f6db288d1cb2bb7c77f871ce4c4ac8d6ea
Rui Guo <rguo@leancloud.rocks>
2018-07-06 07:01:03 +0800

    (feat) file name

--- src/main/java/raft/server/storage/FileName.java
+++ src/main/java/raft/server/storage/FileName.java
@@ -1,16 +1,15 @@
 package raft.server.storage;
 
-import raft.server.PersistentStateException;
-
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.*;
 
 /**
  * Author: ylgrgyq
  * Date: 18/6/24
  */
-public class FileName {
-    public static String getCurrentManifestFileName(String storageName) {
+class FileName {
+    static String getCurrentManifestFileName(String storageName) {
         return storageName + "_CURRENT";
     }
 
@@ -35,15 +34,78 @@ public class FileName {
         try {
             String manifestFileName = getManifestFileName(storageName, manifestFileNumber);
 
-            Files.write(tmpPath, manifestFileName.getBytes(), StandardOpenOption.SYNC,
+            Files.write(tmpPath, manifestFileName.getBytes(StandardCharsets.UTF_8), StandardOpenOption.SYNC,
                     StandardOpenOption.CREATE,
-                    StandardOpenOption.TRUNCATE_EXISTING,
                     StandardOpenOption.WRITE);
 
-            Files.move(tmpPath, Paths.get(baseDir, manifestFileName), StandardCopyOption.REPLACE_EXISTING);
+            Files.move(tmpPath, Paths.get(baseDir, getCurrentManifestFileName(storageName)), StandardCopyOption.REPLACE_EXISTING);
         } catch (IOException ex) {
             Files.deleteIfExists(tmpPath);
             throw ex;
         }
+    }
+
+    static FileNameMeta parseFileName(String fileName) {
+        if (fileName.startsWith("/") || fileName.startsWith("./")) {
+            String[] strs = fileName.split("/");
+            assert strs.length > 0;
+            fileName = strs[strs.length - 1];
+        }
+
+        if (fileName.endsWith("_CURRENT")) {
+            String[] strs = fileName.split("_");
+            assert strs.length == 2;
+            return new FileNameMeta(strs[0], 0, FileType.Current);
+        } else {
+            String[] strs = fileName.split("[\\-\\.]",3);
+            assert strs.length == 3;
+            String storageName = strs[0];
+            int fileNumber = Integer.valueOf(strs[1]);
+            FileType type = FileType.Unknown;
+            switch (strs[2]) {
+                case "sst":
+                    type = FileType.SSTable;
+                    break;
+                case "log":
+                    type = FileType.Log;
+                    break;
+                case "mf":
+                    type = FileType.Manifest;
+                    break;
+            }
+            return new FileNameMeta(storageName, fileNumber, type);
+        }
+    }
+
+    static class FileNameMeta {
+        private final String storageName;
+        private final int fileNumber;
+        private final FileType type;
+
+        FileNameMeta(String storageName, int fileNumber, FileType type) {
+            this.storageName = storageName;
+            this.fileNumber = fileNumber;
+            this.type = type;
+        }
+
+        String getStorageName() {
+            return storageName;
+        }
+
+        int getFileNumber() {
+            return fileNumber;
+        }
+
+        FileType getType() {
+            return type;
+        }
+    }
+
+    enum FileType {
+        Unknown,
+        SSTable,
+        Current,
+        Log,
+        Manifest
     }
 }
