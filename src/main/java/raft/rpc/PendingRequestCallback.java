22c6c2235bcf425eea420df73f3486caa8623cdd
ylgrgyq <alven.gr@gmail.com>
2018-08-05 18:09:18 +0800

    (chore) remove old rpc

--- src/main/java/raft/rpc/PendingRequestCallback.java
+++
@@ -1,9 +0,0 @@
-package raft.rpc;
-
-/**
- * Author: ylgrgyq
- * Date: 17/12/5
- */
-public interface PendingRequestCallback {
-    void operationComplete(PendingRequest request, RemotingCommand response) throws Exception;
-}
